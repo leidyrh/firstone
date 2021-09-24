a26b4ea5e0b7b201995d715fb207824e191c881a
leidyrh <71033251+leidyrh@users.noreply.github.com>
2021-09-24 09:33:42 -0400

    Add files via upload

---
+++ LeidyListTest.java
@@ -0,0 +1,16 @@
+
+public class LeidyListTest {
+	public static void main(String [] args) {
+		LeidyList list = new LeidyList(0);
+		
+		list.add(6);
+		list.add(4);
+		list.add(5);
+		list.add(8);
+		
+		list.print(); 
+		list.clear(2);
+		list.print();
+		list.
+	}
+}
